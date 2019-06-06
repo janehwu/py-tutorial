edfd64197a2d2d5f6c4b72a1984a8011de91eb15
Sita Syal <sitasyal@DN0a22590f.SUNet>
2019-06-06 15:25:43 -0700

    changed dog

--- dog.py
+++ dog.py
@@ -1,22 +1,19 @@
 class Animal(object):
-    def __init__(self, name):
-        self.name = name
+	def __init__(self, name): #This is the constructor
+		self.name = name
 
-class Dog(Animal): #Dog inherits from Animal, e.g. a specific type of Animal
-    num_legs = 4
+class Dog(Animal):
+	num_legs = 4
 
-    # Constructor
-    def __init__(self, name, breed):
-        super(Dog, self).__init__(name)
-        self.breed = breed
-    
-    # Class methods always have self as the first parameter
-    def speak(self):
-        print("woof");
+	def __init__(self, name, breed): 
+		super(Dog, self).__init__(name) # This calls the constructor of the super class, allows us to use name
+		self.breed = breed #This is a unique attribute for the Dog class
+
+	def speak(self):
+		print("woof")
 
 if __name__ == "__main__":
-    d = Dog("Doug", "golden retriever") # Objects are instances of Classes
-    d.speak()
-    print(d.breed)
-    print(d.name)
-    print(d.num_legs)
+	a = Animal("Random")
+	d = Dog("Doug", "Golden Retriever")
+	d.speak()
+	print(d.breed)
